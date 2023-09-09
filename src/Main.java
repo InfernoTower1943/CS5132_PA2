54698edd0f9b4cfe6419a336e406e2f7f4416eea
InfernoTower1943 <nushstalin@gmail.com>
2023-09-09 22:30:20 +0800

    JavaFX installed Added StudentView.fxml: UI for student's view

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("HBox Experiment 1");
+
+        Button button = new Button("My Button");
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("StudentView.fxml"));
+
+        Scene scene = new Scene(root, 1200, 800);
+
+        primaryStage.setTitle("Module Registration");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
 }
--- src/ModulePriorityQueue.java
+++ src/ModulePriorityQueue.java
@@ -1,3 +1,46 @@
-public class ModulePriorityQueue extends PriorityQueue{
+import java.util.Map;
+import java.util.NoSuchElementException;
 
+public class ModulePriorityQueue<T, S extends Comparable<S>> {
+    Map<String, PriorityQueue<T, S>> modulePQMap;
+
+    PriorityQueue<T, S> getPQ(String moduleCode){
+        if (modulePQMap.containsKey(moduleCode)){
+            return modulePQMap.get(moduleCode);
+        }
+        return null;
+    }
+
+    void enqueueToModule(String moduleCode, T item, S priority){
+        if (modulePQMap.containsKey(moduleCode)){
+            modulePQMap.get(moduleCode).enqueue(item, priority);
+        }else{
+            throw new NoSuchElementException("No module with code "+moduleCode+" exists");
+        }
+    }
+
+    PriorityNode<T, S> dequeueFromModule(String moduleCode){
+        if (modulePQMap.containsKey(moduleCode)){
+            PriorityNode<T, S> top = modulePQMap.get(moduleCode).tree.top();
+            modulePQMap.get(moduleCode).dequeue();
+            return top;
+        }
+        return null;
+    }
+
+    void addModule(String moduleCode){
+        if (!modulePQMap.containsKey(moduleCode)){
+            modulePQMap.put(moduleCode, new PriorityQueue<T, S>());
+        }
+    }
+
+    PriorityQueue<T, S> deleteModule(String moduleCode){
+        if (modulePQMap.containsKey(moduleCode)){
+            PriorityQueue<T, S> pq = modulePQMap.get(moduleCode);
+            modulePQMap.remove(moduleCode);
+            return pq;
+        }else{
+            throw new NoSuchElementException("No module with code "+moduleCode+" exists");
+        }
+    }
 }
