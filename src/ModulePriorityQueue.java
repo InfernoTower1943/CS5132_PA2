74ccc9f35a5427bb301ab474aa398f56aa01392d
InfernoTower1943 <nushstalin@gmail.com>
2023-09-10 02:10:54 +0800

    Implemented functionality for time slots within each module inside base files Edited Student UI, ModulePriorityQueue to include time slot parameters: multiple time slots per module Added Pop-up fxml for module order choice popup

--- src/ModulePriorityQueue.java
+++ src/ModulePriorityQueue.java
@@ -1,46 +1,64 @@
+import javafx.util.Pair;
+
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class ModulePriorityQueue<T, S extends Comparable<S>> {
-    Map<String, PriorityQueue<T, S>> modulePQMap;
+    Map<Pair<String, Integer>, PriorityQueue<T, S>> modulePQMap;
 
-    PriorityQueue<T, S> getPQ(String moduleCode){
-        if (modulePQMap.containsKey(moduleCode)){
-            return modulePQMap.get(moduleCode);
+    ArrayList<PriorityQueue<T, S>> getModulePQ(String moduleCode){
+        ArrayList<PriorityQueue<T, S>> result = new ArrayList<>();
+        for (Pair<String, Integer> p : modulePQMap.keySet()){
+            if (p.getKey()==moduleCode){
+                result.add(modulePQMap.get(p));
+            }
+        }
+        return result;
+    }
+
+    PriorityQueue<T, S> getTimeSlotPQ(String moduleCode, Integer timeSlotID){
+        Pair<String, Integer> p = new Pair<>(moduleCode, timeSlotID);
+        if (modulePQMap.containsKey(p)){
+            return modulePQMap.get(p);
         }
         return null;
     }
 
-    void enqueueToModule(String moduleCode, T item, S priority){
-        if (modulePQMap.containsKey(moduleCode)){
-            modulePQMap.get(moduleCode).enqueue(item, priority);
+    void enqueueToTimeSlot(String moduleCode, Integer timeSlotID, T item, S priority){
+        Pair<String, Integer> p = new Pair<>(moduleCode, timeSlotID);
+        if (modulePQMap.containsKey(p)){
+            modulePQMap.get(p).enqueue(item, priority);
         }else{
-            throw new NoSuchElementException("No module with code "+moduleCode+" exists");
+            throw new NoSuchElementException("No time slot with module code "+moduleCode+" and ID "+timeSlotID+"exists");
         }
     }
 
-    PriorityNode<T, S> dequeueFromModule(String moduleCode){
-        if (modulePQMap.containsKey(moduleCode)){
-            PriorityNode<T, S> top = modulePQMap.get(moduleCode).tree.top();
-            modulePQMap.get(moduleCode).dequeue();
+    PriorityNode<T, S> dequeueFromTimeSlot(String moduleCode, Integer timeSlotID){
+        Pair<String, Integer> p = new Pair<>(moduleCode, timeSlotID);
+        if (modulePQMap.containsKey(p)){
+            PriorityNode<T, S> top = modulePQMap.get(p).tree.top();
+            modulePQMap.get(p).dequeue();
             return top;
         }
         return null;
     }
 
-    void addModule(String moduleCode){
-        if (!modulePQMap.containsKey(moduleCode)){
-            modulePQMap.put(moduleCode, new PriorityQueue<T, S>());
+    void addTimeSlot(String moduleCode, Integer timeSlotID){
+        Pair<String, Integer> p = new Pair<>(moduleCode, timeSlotID);
+        if (!modulePQMap.containsKey(p)){
+            modulePQMap.put(p, new PriorityQueue<T, S>());
         }
     }
 
-    PriorityQueue<T, S> deleteModule(String moduleCode){
-        if (modulePQMap.containsKey(moduleCode)){
-            PriorityQueue<T, S> pq = modulePQMap.get(moduleCode);
-            modulePQMap.remove(moduleCode);
+    PriorityQueue<T, S> deleteTimeSlot(String moduleCode, Integer timeSlotID){
+        Pair<String, Integer> p = new Pair<>(moduleCode, timeSlotID);
+        if (modulePQMap.containsKey(p)){
+            PriorityQueue<T, S> pq = modulePQMap.get(p);
+            modulePQMap.remove(p);
             return pq;
         }else{
-            throw new NoSuchElementException("No module with code "+moduleCode+" exists");
+            throw new NoSuchElementException("No time slot with module code "+moduleCode+" and ID "+timeSlotID+"exists");
         }
     }
 }
