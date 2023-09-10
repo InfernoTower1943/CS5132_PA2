0b46cb19422dd2286733241ccdb5ee36b8c9cd05
BTSuns <milesi.alexis@gmail.com>
2023-09-10 21:34:50 +0800

    Added all functions necessary for it to work, just the outline tho.

--- src/Admin/Admin.java
+++ src/Admin/Admin.java
@@ -1,4 +1,29 @@
 package Admin;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 public class Admin {
+    public ArrayList getModuleList(){
+        return null;
+    }
+
+    public PriorityQueue dequeue(){
+        return null;
+    }
+
+    public void setModuleIcon(){
+    }
+
+    public ArrayList getStudentList(){
+        return null;
+    }
+
+    public Object viewStudent(){
+        return null;
+    }
+
+    public void setStudentPriority(){
+
+    }
 }
